23294b70bba5ac2be16e7aaf3a1a660e4b5e1966
Elina <elina8721@gmail.com>
2017-08-26 12:36:37 +0800

    338. Counting Bits Keypoints: (1)Dynamic programming (2)i & (i-1) can eliminate the lowest '1' bit.

--- SingleLinkedList/SingleLinkedList/OtherFunc.cpp
+++ SingleLinkedList/SingleLinkedList/OtherFunc.cpp
@@ -1,4 +1,7 @@
 #include "OtherFunc.h"
+#include <iostream>
+
+using namespace std;
 
 int hammingDistance(int x, int y)
 {
@@ -45,4 +48,21 @@ bool judgeSquareSum(int c)
             return true;
     }
     return false;
+}
+
+// Dynamic Programming:i&(i-1) drops the lowest '1' bit. For example: i = 14, its binary representation is 1110, so i-1 is 1101, i&(i-1) = 1100,
+// the number of "1" in its binary representation decrease one, so ret[i] = ret[i&(i-1)] + 1.
+vector<int> countBits(int num)
+{
+    if (num < 0)
+    {
+        cout << "The input number can not be a negative integer!" << endl;
+    }
+
+    vector<int> ret(num + 1, 0);
+    for (int i = 1; i <= num; i++)
+    {
+        ret[i] = ret[i & (i - 1)] + 1;
+    }
+    return ret;
 }
--- SingleLinkedList/SingleLinkedList/OtherFunc.h
+++ SingleLinkedList/SingleLinkedList/OtherFunc.h
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <vector>
 
 using namespace std;
 /*461. Hamming Distance
@@ -46,3 +47,15 @@ Example 2 :
     Output : False
 */
 bool judgeSquareSum(int c);
+
+/*338. Counting Bits
+Given a non negative integer number num.For every numbers i in the range 0 ¡Ü i ¡Ü num calculate the number of 1's in their binary representation and return them as an array.
+
+Example:For num = 5 you should return[0, 1, 1, 2, 1, 2].
+
+Follow up :
+    It is very easy to come up with a solution with run time O(n*sizeof(integer)).But can you do it in linear time O(n) / possibly in a single pass ?
+    Space complexity should be O(n).
+    Can you do it like a boss ? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+*/
+vector<int> countBits(int num);
