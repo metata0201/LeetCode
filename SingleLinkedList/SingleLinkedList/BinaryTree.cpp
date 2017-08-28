77d98f8c7c82d458aa26ca0efe8f5763f331cead
Elina <elina8721@gmail.com>
2017-08-28 17:03:20 +0800

    637. Average of Levels in Binary Tree Methods: (1)BFS(Breadth First Search) (2)DFS(Depth First Search)

--- SingleLinkedList/SingleLinkedList/BinaryTree.cpp
+++ SingleLinkedList/SingleLinkedList/BinaryTree.cpp
@@ -17,3 +17,66 @@ TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2)
     }
     return root;
 }
+
+// Level order traversal
+vector<double> averageOfLevels(TreeNode* root)
+{
+    vector<double> ret;
+    if (root == nullptr)
+        return ret;
+
+    queue<TreeNode*> Q;
+    Q.push(root);
+    while (!Q.empty())
+    {
+        int num    = Q.size();          // The number of nodes on every level
+        double sum = 0;
+        for (int i = 0; i < num; i++)
+        {
+            TreeNode *pCur = Q.front();
+            sum += pCur->val;
+            if (pCur->left)
+                Q.push(pCur->left);     // Add its left child to the queue
+            if (pCur->right)
+                Q.push(pCur->right);    // Add its right child to the queue
+            Q.pop();
+        }
+        ret.push_back(sum/num);
+    }
+
+    return ret;
+}
+
+// Depth first search:inorder(LVR)
+vector<double> averageOfLevels_DFS(TreeNode* root)
+{
+    vector<double> sum;
+    vector<int> num;
+    average(root, 0, sum, num);
+
+    // Calculate the average on every level
+    for (int i = 0; i < sum.size(); i++)
+    {
+        sum[i] = sum[i] / num[i];
+    }
+    return sum;
+}
+
+void average(TreeNode *pCur, int curLevel, vector<double> &sum, vector<int> &num)
+{
+    if (pCur == nullptr)
+        return;
+
+    if (curLevel < sum.size())
+    {
+        sum[curLevel] += pCur->val;
+        num[curLevel] += 1;
+    }
+    else
+    {
+        sum.push_back(pCur->val);
+        num.push_back(1);
+    }
+    average(pCur->left, curLevel + 1, sum, num);    // Left child
+    average(pCur->right,curLevel + 1, sum, num);    // Right child
+}
