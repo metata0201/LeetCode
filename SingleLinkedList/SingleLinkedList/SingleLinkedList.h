a6d528a59eeaf3f6e41935a46dbf2ad6781df10c
Elina <elina8721@gmail.com>
2017-08-30 14:59:33 +0800

    Revert "521. Longest Uncommon Subsequence I:logical problem, much like a word game."
    This reverts commit 656ad04bd2687c6cd12706660a27fb7aaad0c9a4.

--- SingleLinkedList/SingleLinkedList/SingleLinkedList.h
+++ SingleLinkedList/SingleLinkedList/SingleLinkedList.h
@@ -10,14 +10,6 @@ public:
     ListNode *pNext;
 };
 
-// Definition for singly-linked list with a random pointer.
-struct RandomListNode
-{
-    int label;
-    RandomListNode *next, *random;
-    RandomListNode(int x) : label(x), next(nullptr), random(nullptr) {}
-};
-
 class SingleLinkedList
 {
 public:
